c1297b42f543ddc6b913fceb55f1c418286f0cbf
melovagabond <daevon.branche@fmc.com>
2025-11-21 18:36:02 -0500

    added basic fixes for imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './main.tsx'
+import MusicCodexApp from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <MusicCodexApp />
   </React.StrictMode>,
 )
